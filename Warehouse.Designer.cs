977bcabc1681cc1c1ede02641ba6dceadb19b64a
Suriya AES <suriyam231@gmail.com>
2019-12-12 16:14:52 +0700

    center app

--- Warehouse.Designer.cs
+++ Warehouse.Designer.cs
@@ -28,20 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(1, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1440, 100);
+            this.label1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("TH SarabunPSK", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(16, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 79);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "เพิ่มสินค้า";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Warehouse
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1424, 861);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
             this.Name = "Warehouse";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Warehouse";
-            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Warehouse_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Warehouse.cs
+++ Warehouse.cs
@@ -12,9 +12,26 @@ namespace POSTiw
 {
     public partial class Warehouse : Form
     {
+
+        List<Panel> listPanal = new List<Panel>();
+        int index;
         public Warehouse()
         {
             InitializeComponent();
+
+         
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void Warehouse_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+       
     }
 }
