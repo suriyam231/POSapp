9ff752e721ea974d052c8285cdb81136bfc47263
Suriya AES <suriyam231@gmail.com>
2020-01-08 14:53:02 +0700

    สินค้าไกล้หมด

--- LowProduct.cs
+++ LowProduct.cs
@@ -21,15 +21,28 @@ namespace POSTiw
             textBox1.Text = "10";
             int number = 0;
             Int32.TryParse(textBox1.Text.ToString(), out number);
+
             SqlCommand command = new SqlCommand();
             command.Connection = sqlcon;
-            command.CommandText = "select ProductID , ProductName , TypeName, ProductQuantity , ProductFrom from Products where ProductQuantity < "+number+"";
+            command.CommandText = "select ProductID , ProductName , TypeName, ProductQuantity , ProductFrom from Products where ProductQuantity <= "+number+ " ORDER BY ProductQuantity";
 
             DataTable data = new DataTable();
             SqlDataAdapter adapter = new SqlDataAdapter(command);
 
             adapter.Fill(data);
             dataGridView1.DataSource = data;
+
+            sqlcon.Open();
+            string qry = "SELECT * FROM TypeProduct";
+
+            SqlDataReader reader = new SqlCommand(qry, sqlcon).ExecuteReader();
+            while (reader.Read())
+            {
+                comboBox1.Items.Add(reader.GetValue(1).ToString());
+            }
+            sqlcon.Close();
+
+
         }
 
         private void button5_Click(object sender, EventArgs e)
@@ -55,11 +68,110 @@ namespace POSTiw
             dataGridView1.Columns[3].Width = 100;
             dataGridView1.Columns[4].Width = 100;
 
+
+            LaId.Hide();
+            LaName.Hide();
+            LaQu.Hide();
+            LaType.Hide();
+            LaType2.Hide();
+            LaUp.Hide();
+            LaInput.Hide();
+            Labutton.Hide();
+
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+            if (comboBox1.SelectedItem == null)
+            {
+                SqlCommand command = new SqlCommand();
+                int number = 0;
+                Int32.TryParse(textBox1.Text.ToString(), out number);
+                command.Connection = sqlcon;
+                command.CommandText = "SELECT ProductID , ProductName , TypeName, ProductQuantity , ProductFrom FROM Products WHERE ProductQuantity <= " + number + " ORDER BY ProductQuantity ";
+                DataTable data = new DataTable();
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(data);
+                dataGridView1.DataSource = data;
+            }
+            else if (comboBox1.SelectedItem != null)
+            {
+                try
+                {
+                    SqlCommand command2 = new SqlCommand();
+                    command2.Connection = sqlcon;
+                    command2.CommandText = "SELECT ProductID , ProductName , TypeName, ProductQuantity , ProductFrom FROM [dbo].[Products] WHERE TypeName = '" + comboBox1.SelectedItem.ToString() + "' and ProductQuantity <= " + textBox1.Text.ToString() + "  ORDER BY ProductQuantity";
+
+                    DataTable data2 = new DataTable();
+                    SqlDataAdapter adapter2 = new SqlDataAdapter(command2);
+
+                    adapter2.Fill(data2);
+                    dataGridView1.DataSource = data2;
+                }
+                catch(Exception er)
+                {
+
+                }
+            }
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string values = comboBox1.SelectedItem.ToString();
+            int number = 0;
+            Int32.TryParse(textBox1.Text.ToString(), out number);
+            SqlCommand command = new SqlCommand();
+            command.Connection = sqlcon;
+            command.CommandText = "SELECT ProductID , ProductName , TypeName, ProductQuantity , ProductFrom FROM [dbo].[Products] WHERE TypeName = '" + values + "' and ProductQuantity <= "+number+ "  ORDER BY ProductQuantity";
+
+            DataTable data = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+            adapter.Fill(data);
+            dataGridView1.DataSource = data;
+        }
+        public void Onkey(object sender, KeyEventArgs kea)
+        {
+
+            if (comboBox1.SelectedItem == null)
+            {
+                int number = 0;
+                Int32.TryParse(textBox1.Text.ToString(), out number);
+                SqlCommand command = new SqlCommand();
+                command.Connection = sqlcon;
+                command.CommandText = "SELECT ProductID , ProductName , TypeName, ProductQuantity , ProductFrom FROM Products WHERE ProductQuantity <= " + number + " ORDER BY ProductQuantity ";
+
+                DataTable data = new DataTable();
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+                adapter.Fill(data);
+                dataGridView1.DataSource = data;
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LaId.Show();
+            LaName.Show();
+            LaQu.Show();
+            LaType.Show();
+            LaType2.Show();
+            LaUp.Show();
+            LaInput.Show();
+            Labutton.Show();
         }
     }
 }
